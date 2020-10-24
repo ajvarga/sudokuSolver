b6d9ad7a10c916be5fadb14e194b186fc5296b2a
Alexander J Vargas <56698261+ajvarga@users.noreply.github.com>
2020-10-23 23:50:16 -0700

    added a printer function to visualize board

--- solver.py
+++ solver.py
@@ -1,4 +1,4 @@
-#first make a sudoku board, its a 9x9 matrix
+#first make a sudoku board, its a 9x9 matrix. 0 represents a blank space
 board = [
     [5,3,0,0,7,0,0,0,0],
     [6,0,0,1,9,5,0,0,0],
@@ -10,3 +10,19 @@ board = [
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,8,0,0,7,9]
 ]
+# print out the board, formatting. helps visualize it for the future
+
+def printBoard(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print('- - - - - - - - - - - - -')
+        for j in range(len(board)):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end='')
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) +  ' ', end='')
+            
+# print_board(board)
